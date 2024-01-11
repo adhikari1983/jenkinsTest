c2e7624bfbcfe64cf405321084948f9c0ec9c3eb
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2024-01-11 15:02:59 -0500

    updated project name

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Running the trigger-333 in jenkins..");
+            System.out.println("Running the trigger-333 5 times in the jenkins server..");
         }
     }
 }
