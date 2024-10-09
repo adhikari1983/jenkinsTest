4328dd88b1fdc06e7753ef34c85ba3a9bf135e34
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2024-10-09 18:24:32 -0400

    updated project name code edit

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,8 @@ public class Test {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("Running the trigger-333 5 times in the jenkins server..");
+            System.out.println("Running on my local machine jenkins server ");
+
         }
     }
 }
